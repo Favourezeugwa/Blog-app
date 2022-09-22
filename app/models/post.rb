d7dafe7960cd4e0a8ad2e0898073f609f34c18af
Favourezeugwa <favourezeugwa@gmail.com>
2022-09-22 14:52:31 +0100

    Add custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :user
+
+  def update_post_counter
+    user.increment!(:post_counter)
+  end
+
+  def return_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
