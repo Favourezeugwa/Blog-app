01047b6bb51cd18256f37796eacb00f08251585c
Favourezeugwa <favourezeugwa@gmail.com>
2022-09-23 09:12:03 +0100

    update private methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,11 @@ class Post < ApplicationRecord
   belongs_to :users, foreign_key: 'user_id', class_name: 'User'
   after_save :update_posts_counter
 
-  def return_recent_comments
+  def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  private
 
   def update_posts_counter
     users.increment!(:posts_counter)
