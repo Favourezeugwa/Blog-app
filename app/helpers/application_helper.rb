3ade1debcc3eddda56f0589eac29e85f842a6fb7
Favourezeugwa <favourezeugwa@gmail.com>
2022-09-30 14:21:28 +0100

    add validations to counters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def available_comments?(post)
+    if post.comments_counter.zero?
+      'No comments added!'
+    else
+      post.comments
+    end
+  end
+
+  def available_posts(user)
+    if user.posts_counter.zero?
+      'No posts added!'
+    else
+      user.posts
+    end
+  end
 end
