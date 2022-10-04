871ad30b1625f564328a4e288d0661588b0e943a
Favourezeugwa <favourezeugwa@gmail.com>
2022-10-04 01:14:03 +0100

    Add user_show spec

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -1,35 +1,79 @@
 require 'rails_helper'
 
-RSpec.describe 'User Show' do
+RSpec.describe 'User Show', type: :feature do
   before(:each) do
     @user1 = User.create(
-      name: 'Favour',
-      bio: 'Fashion Designer',
-      photo: 'http://hello.com',
-      posts_counter: 2
+      name: 'Amarachi',
+      bio: 'Product Manager',
+      photo: 'http://product.com',
+      posts_counter: 0
     )
 
     @post1 = Post.create(
-      user_id: @user1.id,
+      users: @user1,
       title: 'First post',
       text: 'This is my first post',
       comments_counter: 1,
-      likes_counter: 2
+      likes_counter: 3
     )
 
     @post2 = Post.create(
-      user_id: @user1.id,
+      users: @user1,
       title: 'Second post',
       text: 'This is my second post',
-      comments_counter: 1,
+      comments_counter: 2,
       likes_counter: 2
     )
   end
 
   describe 'a user show page' do
-    it 'displays the user\'s profile photo' do
-      visit user_path
-      expect(page).to have_css("img[src*='http://hello.com']")
+    it 'displays the users profile photo' do
+      visit user_path(@user1)
+      expect(page).to have_css("img[src*='http://product.com']")
     end
+  end
+
+  it 'displays the user name' do
+    visit user_path(@user1)
+    expect(page).to have_content('Amarachi')
+  end
+
+  it 'displays the user bio' do
+    visit user_path(@user1)
+    expect(page).to have_content('Product Manager')
+  end
+
+  it 'shows the right Number of posts' do
+    visit user_path(@user1)
+    expect(page).to have_content('Number of Posts: 2')
+  end
+
+  it 'shows all the available users posts' do
+    visit user_path(@user1)
+    expect(page).to have_content('First post')
+    expect(page).to have_content('Second post')
+  end
+
+  it 'shows a button that helps view all of a users posts' do
+    visit user_path(@user1)
+    expect(page).to have_content('see all posts')
+  end
+
+  it 'click to see all posts, it redirects me to the users posts index page' do
+    visit user_path(@user1)
+    click_link 'see all posts'
+    expect(page).to have_current_path(user_posts_path(@user1))
+  end
+
+  it 'click a users post, it redirects me to that posts show page' do
+    visit user_path(@user1)
+    click_link 'First post'
+    expect(page).to have_current_path(user_post_path(@user1, @post1))
+  end
+
+  it 'click a users post, it redirects me to that posts show page' do
+    visit user_path(@user1)
+    click_link 'Second post'
+    expect(page).to have_current_path(user_post_path(@user1, @post2))
   end
 end
