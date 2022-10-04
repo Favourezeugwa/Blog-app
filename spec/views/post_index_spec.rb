9cdf35b2488cfd24d7b2ab02da45e44686f131b4
Yohannes(Jo) <yohannesdagnachew2020@gmail.com>
2022-10-04 12:06:50 +0300

    displays total posts by the user

--- spec/views/post_index_spec.rb
+++ spec/views/post_index_spec.rb
@@ -32,5 +32,37 @@ RSpec.describe 'Post Index', type: :feature do
     Like.create(post_id: @post1.id, user_id: @user1.id)
     Like.create(post_id: @post2.id, user_id: @user1.id)
   end
+  describe 'a post index page' do
+    it 'displays the user name' do
+      visit user_posts_path(@user1)
+      expect(page).to have_content('Amarachi')
+    end
 
+    it 'displays the user photo' do
+      visit user_posts_path(@user1)
+      expect(page).to have_css("img[src*='http://product.com']")
+    end
+
+    it 'displays total posts by the user' do
+      visit user_posts_path(@user1)
+      expect(page).to have_content('Number of Posts: 2')
+    end
+
+    it 'can see some of the post body' do
+      visit user_posts_path(@user1)
+      expect(page).to have_content('First post')
+      expect(page).to have_content('Second post')
+      expect(page).to have_content('This is my first post')
+      expect(page).to have_content('This is my second post')
+      expect(page).to have_content('add your comments ...')
+      expect(page).to have_selector(:link_or_button, 'Like post')
+      expect(page).to have_selector(:link_or_button, 'Add comment')
+    end
+
+    it 'shows how many comments and likes a post has' do
+      visit user_posts_path(@user1)
+      expect(page).to have_content('Comments: 2, Likes: 1')
+      expect(page).to have_content('Comments: 1, Likes: 1')
+    end
+  end
 end
