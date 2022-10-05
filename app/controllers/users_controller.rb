b9b33a691c7f877f197a3a24473e25b5b2bb28a3
Favourezeugwa <favourezeugwa@gmail.com>
2022-10-05 23:42:41 +0100

    fix trailing spaces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @users = User.all
   end
 
   def show
-    @user =  current_user
+    @user = current_user
   end
 end
