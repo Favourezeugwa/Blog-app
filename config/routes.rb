e32be0faa62f476ed273caab7304b6e8f235b7cf
Favourezeugwa <favourezeugwa@gmail.com>
2022-10-07 10:25:05 +0100

    Modify routing for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "users#show"
+  root to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
